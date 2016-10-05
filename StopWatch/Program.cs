bba3b6e487be570b700a80ecccdaac9bccc62691
Xiaocao Hu <sylvie910@gmail.com>
2016-10-05 13:17:37 -0400

    initial commit

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace StopWatch
 {
@@ -10,6 +7,43 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
+            var stopwatch = new StopWatch();
+            Console.WriteLine("Press enter to begin the stopwatch");
+            Console.ReadLine();
+            stopwatch.Start();
+            Thread.Sleep(1000);
+            stopwatch.End();
+            Console.WriteLine($"Duration:  {stopwatch.GetInterval()}");
+            Console.ReadLine();
+        }
+    }
+    class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isRunning;
+
+        public void Start()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Stopwatch is already running");
+
+            _startTime = DateTime.Now;
+            _isRunning = true;
+        }
+
+        public void End()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("Stopwatch is not running");
+
+            _endTime = DateTime.Now;
+            _isRunning = false;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
         }
     }
 }
